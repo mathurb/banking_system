7f7ce71ee0e77dbdb98a95787b5fb57f96ff6b8c
bhavya mathur <jmathur8@gmail.com>
2018-10-07 23:41:00 +0530

    spelling_mistakes_corrected

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,5 +1,5 @@
 class Loan < ApplicationRecord
-    belongs_to :user
+    belongs_to :client
     validates :loan_type, presence: true, length: {maximum: 10}
     validates :amount, presence: true, length: {maximum: 10}, numericality: true
 end
