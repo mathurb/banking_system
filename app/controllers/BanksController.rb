c9703768ffcfb403a76b0f6ed5f172d8285f5e94
bhavya mathur <jmathur8@gmail.com>
2018-10-10 21:05:05 +0530

    controllers and routes

---
+++ app/controllers/BanksController.rb
@@ -0,0 +1,95 @@
+class BanksController < ApplicationController
+
+skip_before_action :verify_authenticity_token
+
+ def show
+ begin
+   @bank = Bank.find(params[:id])
+   respond_to do |format|
+   format.json { render json: {bank:@bank}, status: :ok }
+	end
+   rescue ActiveRecord::RecordNotFound => e
+   respond_to do |format|
+   format.json { render json: {error:e.message}, status: :not_found}
+	end
+   end
+ end
+	
+ def new
+  @bank = Bank.new
+  respond_to do |format|
+  format.json { render json: {bank: @bank}, status: :ok }
+   end
+ end
+	
+def create
+ begin
+ @bank = Bank.new(bank_params)
+ respond_to do |format|
+ if @bank.save
+  format.json { render json: { bank: @bank}, status: :created }
+ else
+  format.json { render json: @bank.errors, status: :unprocessable_entity }
+ end
+ end
+ rescue ActiveRecord::InvalidForeignKey => e
+  respond_to do |format|
+  format.json { render json: {error:'Invalid Foreign Key'}, status: :unprocessable_entity }
+  end
+ end
+end
+
+
+def index
+ @bank = Bank.all
+ respond_to do |format|
+ format.json { render json: {bank:@bank}, status: :ok }
+ end
+end
+
+def edit
+ begin
+ @bank = Bank.find(params[:id])
+ respond_to do |format|
+ format.json { render json: {bank:@bank}, status: :ok }
+  end
+ rescue ActiveRecord::RecordNotFound => e
+ respond_to do |format|
+ format.json { render json: {error:e.message}, status: :not_found }
+  end
+ end
+end
+
+def destroy
+		begin
+			@bank = Bank.find(params[:id])
+			respond_to do |format|
+			  @bank.destroy
+				format.json { render json: {}, status: :ok }
+			end  
+		rescue ActiveRecord::RecordNotFound => e
+			respond_to do |format|
+				format.json { render json: {error:e.message}, status: :unprocessable_entity }
+			end
+		end
+	end
+
+def update
+ begin
+ @bank = Bank.find(params[:id])
+ respond_to do |format|
+ if @bank.update(bank_params)
+  format.json { render json: {bank:@bank}, status: :ok }
+ else
+  format.json { render json: @bank.errors, status: :unprocessable_entity }
+ end
+end
+rescue => e
+ respond_to do |format|
+ format.json { render json: {error:e.message}, status: :unprocessable_entity }
+ end
+ end
+end
+
+
+end	
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :accounts
+  resources :bank
+  resources :branches
+  resources :cards
+  resources :clients
+  resources :loans 
+  resources :transections
 end
